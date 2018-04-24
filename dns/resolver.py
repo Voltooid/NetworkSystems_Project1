5c9a178dadec6c2e239be9f8baeefc05c442203c
rensalthuis <rens.althuis@gmail.com>
2018-04-24 15:36:20 +0200

    cleanup resolver code

--- dns/resolver.py
+++ dns/resolver.py
@@ -30,6 +30,71 @@ class Resolver:
         self.caching = caching
         self.ttl = ttl
 
+    def logHeader(self, header):
+            print("\tFLAGS", end="")
+            print(" QR", header.qr, end=";")
+            print(" OPCODE", header.opcode, end=";")
+            print(" AA", header.aa, end=";")
+            print(" TC", header.tc, end=";")
+            print(" RD", header.rd, end=";")
+            print(" RA", header.ra, end=";")
+            print(" Z", header.z, end=";")
+            print(" RCODE", header.rcode, end=";\n")
+
+    def send_request(self, ip, name):
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.settimeout(self.timeout)
+
+            question = Question(name, Type.A, Class.IN)
+            header = Header(9001, 0, 1, 0, 0, 0)
+            header.qr = 0
+            header.opcode = 0
+            header.rd = 0
+            query = Message(header, [question])
+
+            sock.sendto(query.to_bytes(), (ip, 53))
+
+            # Receive response
+            data = sock.recv(512)
+            response = Message.from_bytes(data)
+            self.logHeader(response.header)
+            return response.answers, response.authorities, response.additionals
+
+    def resolve_request(self, ip, hostname):
+            print("\nRESOLVING REQUEST", hostname, "at:", ip)
+            answers, authorities, additionals = self.send_request(ip, hostname)
+
+            # Process data
+            namelist = []
+            ipaddrlist = []
+            if len(answers) != 0:
+                print("\n\tGOT RESPONSE")
+                for answer in answers:
+                    if answer.type_ == Type.A:
+                        ipaddrlist.append(answer.rdata.address)
+                        print("\t\t", answer.type_, answer.rdata.address)
+                    if answer.type_ == Type.CNAME:
+                        namelist.append(hostname)
+                        print("\t\t", answer.type_, answer.rdata.cname)
+                return True, ipaddrlist, namelist
+            else:
+
+                if len(additionals) != 0:
+                    print("\n\tGOT ADDITIONALS")
+                    for answer in additionals:
+                        if answer.type_ == Type.A:
+                            ipaddrlist.append(answer.rdata.address)
+                            print('\t\t', answer.type_, answer.rdata.address)
+
+                if len(authorities) != 0:
+                    print("\n\tGOT AUTHORITIES")
+                    for answer in authorities:
+                        namelist.append(answer.rdata.nsdname)
+                        print('\t\t', answer.type_, answer.rdata.nsdname)
+
+                return False, ipaddrlist, namelist
+
     def gethostbyname(self, hostname):
         """Translate a host name to IPv4 address.
 
@@ -43,7 +108,6 @@ class Resolver:
             (str, [str], [str]): (hostname, aliaslist, ipaddrlist)
         """
         # check cache
-        
         # pick nameserver
 
 
@@ -52,83 +116,24 @@ class Resolver:
 
         hostname = Name(hostname)
         print("NEW QUERY:", hostname)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(self.timeout)
         rootserverip = "198.41.0.4"
         serveriplist = [rootserverip]
-        servernamelist = [hostname]
 
-        while len(serveriplist) != 0 or len(servernamelist) != 0:
-            # Create and send query
-            ip = serveriplist[0]
-            if not (len(servernamelist) > 1):
-                del serveriplist[0]
-            servername = servernamelist[0]
-            if not (len(serveriplist) > 1):
-                del servernamelist[0]
-
-            print("\nREQUEST", servername, "FROM:", ip)
-            question = Question(servername, Type.A, Class.IN)
-            header = Header(9001, 0, 1, 0, 0, 0)
-            header.qr = 0
-            header.opcode = 0
-            header.rd = 0
-            query = Message(header, [question])
-
-            sock.sendto(query.to_bytes(), (ip, 53))
-
-            # Receive response
-            data = sock.recv(512)
-            response = Message.from_bytes(data)
-
-            # Get data
-            aliaslist = []
-            ipaddrlist = []
-            print("\tFLAGS", end="")
-            print(" QR", response.header.qr, end=";")
-            print(" OPCODE", response.header.opcode, end=";")
-            print(" AA", response.header.aa, end=";")
-            print(" TC", response.header.tc, end=";")
-            print(" RD", response.header.rd, end=";")
-            print(" RA", response.header.ra, end=";")
-            print(" Z", response.header.z, end=";")
-            print(" RCODE", response.header.rcode, end=";\n")
-
-            if len(response.answers) != 0:
-                print("\n\tGOT RESPONSE")
-                servernamelist = []
-                serveriplist = []
-                for answer in response.answers:
-                    if answer.type_ == Type.A:
-                        if servername == hostname:
-                            ipaddrlist.append(answer.rdata.address)
-                        else:
-                            serveriplist = [answer.rdata.address]
-                            servernamelist = [hostname]
-                        print("\t\t", answer.type_, answer.rdata.address)
-                    if answer.type_ == Type.CNAME:
-                        aliaslist.append(hostname)
-                        hostname = str(answer.rdata.cname)
-                        print("\t\t", answer.type_, answer.rdata.cname)
-
-            if len(response.additionals) != 0: 
-                print("\n\tGOT ADDITIONALS")
-                serveriplist = []
-                servernamelist = [servername]
-                for answer in response.additionals:
-                    if answer.type_ == Type.A:
-                        serveriplist.append(answer.rdata.address)
-                        print('\t\t', answer.type_, answer.rdata.address)
-
-            elif len(response.authorities) != 0:
-                print("\n\tGOT AUTHORITIES")
-                serveriplist = [rootserverip]
-                servernamelist = []
-                for answer in response.authorities:
-                    servernamelist.append(answer.rdata.nsdname)
-                    print('\t\t', answer.type_, answer.rdata.nsdname)
-            print("END")
-
-
-        print("END OF QUERY:", hostname)
-        return hostname, aliaslist, ipaddrlist
+        while len(serveriplist) != 0:
+            res, iplist, namelist = self.resolve_request(serveriplist[0], hostname)
+            if res:
+                print("END OF QUERY:", hostname)
+                return hostname, namelist, iplist
+            elif len(iplist) == 0 and not len(namelist) == 0:
+                newlist = []
+                for x in namelist:
+                    newhostname, newaliases, newips= self.gethostbyname(str(x))
+                    print("newIPS", newips)
+                    newlist.extend(newips)
+                newlist.extend(serveriplist)
+                serveriplist = newlist
+            else:
+                iplist.extend(serveriplist[1:])
+                serveriplist = iplist
+            print(serveriplist)
+        print("FAILURE")
